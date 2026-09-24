bfd0240ca732f4272cfd11fd3d2d7ca7bbdafaad
clashcreativectg <clashcreative123@gmail.com>
2026-09-24 18:26:42 -0500

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -1,2 +1,2 @@
 // Cambia esta URL por la dirección de tu Cloudflare Worker.
-window.LIMITVERSE_API = "limitverse-api.clashcreative123.workers.dev";
+window.LIMITVERSE_API = "https://limitverse-api.clashcreative123.workers.dev";
